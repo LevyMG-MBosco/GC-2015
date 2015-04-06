4cd88d7b909f5d8dc05b5280d0ed8037b3a45c0d
Michael Bosco <mike@levymgi.com>
2015-04-06 15:12:51 -0400

    changed the placeholders to something tidier

--- interior.php
+++ interior.php
@@ -7,7 +7,7 @@
           <img class="img-responsive" />
       </div>
       <div class="interior-content">
-        <img src="assets/imgs/AmmoHeader.jpg" />
+        <img src='http://placehold.it/780x162' />
         <h1>Combining Experience & Enthusiasm to Serve You Better</h1>
         <p>Since 1968, we’ve been supplying industry with cemented carbide for the most demanding industrial wear and metal forming applications. Today, we offer more than 50 grades of cemented carbide for applications ranging from pure abrasion to high impingement erosive wear to chemical/mechanical corrosive wear, including applications that involve high-impact strength.</p>
         <p>Our experienced team of application engineers and market managers can recommend Sinter-HIP grade formulations that best fit each customer’s application. With manufacturing capabilities that ensure high quality and reliable delivery, General Carbide is your best source for cemented carbide, high binder cobalt grades and more.</p>
@@ -23,13 +23,14 @@
           <span><li id="nav4">General Metalforming</li></span>
           <span><li id="nav5">Oil & Gas Components</li></span>
           <span><li id="nav6">PM/Compaction Tooling</li></span>
-          <span><li id="nav7">Tooftile Tooling</li></span>
-          <span><li id="nav8">Rotary Tooling</li></span>
-          <span><li id="nav9">Severe Service Tooling</li></span>
-          <span><li id="nav10">Specialty Tooling</li></span>
-          <span><li id="nav11">Stamping/Lamination Tooling</li></span>
-          <span><li id="nav12">Synthetic Diamond<br /> Manufacturing</li></span>
-          <span><li id="nav13">Wire Drawing Tooling</li></span>
+          <span><li id="nav7">Rooftile Tooling</li></span>
+          <span><li id="nav8">Rotary Cutting Dies</li></span>
+          <span><li id="nav9">Rotary Tooling</li></span>
+          <span><li id="nav10">Severe Service Tooling</li></span>
+          <span><li id="nav11">Specialty Tooling</li></span>
+          <span><li id="nav12">Stamping/Lamination Tooling</li></span>
+          <span><li id="nav13">Synthetic Diamond<br /> Manufacturing</li></span>
+          <!--<span><li id="nav13">Wire Drawing Tooling</li></span>-->
         </ul>
       </div>
     </div>
