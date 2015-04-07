ff48d1db7a99e1d2e8c089a2e3c92a69eda55452
Michael Bosco <mike@levymgi.com>
2015-04-07 16:53:21 -0400

    Interior page builtout progress for the day

--- grades.php
+++ grades.php
@@ -8,30 +8,18 @@
       </div>
       <div class="interior-content">
         <img src='http://placehold.it/780x162' />
-        <h1>Combining Experience & Enthusiasm to Serve You Better</h1>
-        <p>Since 1968, we’ve been supplying industry with cemented carbide for the most demanding industrial wear and metal forming applications. Today, we offer more than 50 grades of cemented carbide for applications ranging from pure abrasion to high impingement erosive wear to chemical/mechanical corrosive wear, including applications that involve high-impact strength.</p>
-        <p>Our experienced team of application engineers and market managers can recommend Sinter-HIP grade formulations that best fit each customer’s application. With manufacturing capabilities that ensure high quality and reliable delivery, General Carbide is your best source for cemented carbide, high binder cobalt grades and more.</p>
-        <p>Having been in business for more than 45 years, we still approach every customer’s challenge with unmatched enthusiasm. Let us show you how we can meet your industrial wear requirements and help you enhance productivity.</p>
+        <h1>Grades</h1>
+        <h3>Making the Grades</h3>
+        <p>We produce more than 50 carbide grades from cobalt, nickel, tantalum and tungsten. They come in a wide range of binder concentrations and particles sizes. Our tungsten carbide grades use cobalt binders ranging in concentrations up to 30%, and offer nominal particle sizes from 0.55 microns to 10 microns. The superfine carbides are ideal for applications requiring sharp edges, and our broad range of tungsten carbide with tantalum reduce galling in your metal-forming operations.</p>
+        <p>As a major producer of nickel carbides for environments where corrosion, oxidation and wear push your tooling to the limit, we maintain the purity of our carbides through a dedicated nickel room, and through nickel production processes that eliminate the potential for contamination.</p>
+        <p>Our unique combination of carbide grades, binder concentrations and production controls enables us to meet the most demanding material specifications.</p>
       </div>
     </div>
     <div class="col-xs-4">
       <div class="interior-nav">
         <ul class="nav">
-          <span><li id="nav0" class="selected">Industries</li></span>
-          <span><li id="nav1">Ammunition Tooling &<br /> Firearms Manufacturing</li></span>
-          <span><li id="nav2">Can Tooling</li></span>
-          <span><li id="nav3">Fastener Tooling</li></span>
-          <span><li id="nav4">General Metalforming</li></span>
-          <span><li id="nav5">Oil & Gas Components</li></span>
-          <span><li id="nav6">PM/Compaction Tooling</li></span>
-          <span><li id="nav7">Rooftile Tooling</li></span>
-          <span><li id="nav8">Rotary Cutting Dies</li></span>
-          <span><li id="nav9">Rotary Tooling</li></span>
-          <span><li id="nav10">Severe Service Tooling</li></span>
-          <span><li id="nav11">Specialty Tooling</li></span>
-          <span><li id="nav12">Stamping/Lamination Tooling</li></span>
-          <span><li id="nav13">Synthetic Diamond<br /> Manufacturing</li></span>
-          <!--<span><li id="nav13">Wire Drawing Tooling</li></span>-->
+          <span><li id="nav0" class="selected">Grades</li></span>
+          <span><li id="nav1">About Carbide</li></span>
         </ul>
       </div>
     </div>
--- quality.php
+++ quality.php
@@ -8,30 +8,30 @@
       </div>
       <div class="interior-content">
         <img src='http://placehold.it/780x162' />
-        <h1>Combining Experience & Enthusiasm to Serve You Better</h1>
-        <p>Since 1968, we’ve been supplying industry with cemented carbide for the most demanding industrial wear and metal forming applications. Today, we offer more than 50 grades of cemented carbide for applications ranging from pure abrasion to high impingement erosive wear to chemical/mechanical corrosive wear, including applications that involve high-impact strength.</p>
-        <p>Our experienced team of application engineers and market managers can recommend Sinter-HIP grade formulations that best fit each customer’s application. With manufacturing capabilities that ensure high quality and reliable delivery, General Carbide is your best source for cemented carbide, high binder cobalt grades and more.</p>
-        <p>Having been in business for more than 45 years, we still approach every customer’s challenge with unmatched enthusiasm. Let us show you how we can meet your industrial wear requirements and help you enhance productivity.</p>
+        <h1>Quality</h1>
+        <h2>ISO-Certified to Meet Your Quality Requirements</h2>
+        <p>At General Carbide, we’ve invested in the quality systems and process needed to become ISO-certified as of June 2014. That’s because we’re committed to meeting the highest standards in everything we do. We achieve a superior level of quality by using:</p>
+        <ul>
+          <li>CAD/CAM systems that add speed and accuracy to the early stages of production</li>
+          <li>CNC machines that allow us to make complex shapes with superior dimensional control</li>
+          <li>Inspections throughout production using coordinate measuring machines</li>
+          <li>Packaging systems that help to ensure your shipment arrives in perfect condition</li>
+        </ul>
+        <p>Our quality policy is simple, yet powerful, and consists of:</p>
+        <ul>
+          <li>Meeting or exceeding customers’ requirements for quality and performance</li>
+          <li>Manufacturing and shipping zero-defect products on-time</li>
+          <li>Cultivating a culture of continuous improvement</li>
+        </ul>
+        <p>Whether your requirements are complex or clear-cut, General Carbide has the engineering expertise, quality control and value pricing to become your single source for preferred supplier of components and assemblies for tungsten carbide tooling used in a variety of industrial wear and metal forming applications.</p>
       </div>
     </div>
     <div class="col-xs-4">
       <div class="interior-nav">
         <ul class="nav">
-          <span><li id="nav0" class="selected">Industries</li></span>
-          <span><li id="nav1">Ammunition Tooling &<br /> Firearms Manufacturing</li></span>
-          <span><li id="nav2">Can Tooling</li></span>
-          <span><li id="nav3">Fastener Tooling</li></span>
-          <span><li id="nav4">General Metalforming</li></span>
-          <span><li id="nav5">Oil & Gas Components</li></span>
-          <span><li id="nav6">PM/Compaction Tooling</li></span>
-          <span><li id="nav7">Rooftile Tooling</li></span>
-          <span><li id="nav8">Rotary Cutting Dies</li></span>
-          <span><li id="nav9">Rotary Tooling</li></span>
-          <span><li id="nav10">Severe Service Tooling</li></span>
-          <span><li id="nav11">Specialty Tooling</li></span>
-          <span><li id="nav12">Stamping/Lamination Tooling</li></span>
-          <span><li id="nav13">Synthetic Diamond<br /> Manufacturing</li></span>
-          <!--<span><li id="nav13">Wire Drawing Tooling</li></span>-->
+          <span><li id="nav0" class="selected">Quality</li></span>
+          <span><li id="nav1">Sinter-HIP Advantages</li></span>
+          <span><li id="nav2">Quality Policy</li></span>
         </ul>
       </div>
     </div>
