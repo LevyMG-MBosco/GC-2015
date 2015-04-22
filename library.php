f599a0c99c79a70edeea6b20a42ba99691ed9d95
Michael Bosco <mike@levymgi.com>
2015-04-22 10:44:16 -0400

    The last of these pdfs

--- library.php
+++ library.php
@@ -30,12 +30,12 @@
             </div>
           </div>
           <div class="row">
-            <div class="col-xs-12 col-sm-9">
+            <div class="technical-links col-xs-12 col-sm-9">
               <h3>Sales Literature</h3>
 
-              <p><a href="assets/pdf/GCBrochure.pdf">Capabilities Brochure</a></p>
-              <p><a href="assets/pdf/GCGradeSpecs.pdf">Grade Specification Chart</a></p>
-              <p><a href="assets/pdf/GCMSDS.pdf">Material Safety Data Sheets</a></p>
+              <p><a href="assets/pdf/GCBrochure.pdf" target="_blank">Capabilities Brochure</a></p>
+              <p><a href="assets/pdf/GCGradeSpecs.pdf" target="_blank">Grade Specification Chart</a></p>
+              <p><a href="assets/pdf/GCMSDS.pdf" target="_blank">Material Safety Data Sheets</a></p>
 
             </div>
             <div class="col-xs-12 col-sm-3">
@@ -49,19 +49,19 @@
             </div>
           </div>
           <div class="row">
-            <div class="col-xs-12 col-sm-9">
+            <div class="technical-links col-xs-12 col-sm-9">
               <h3>Application Data Sheets</h3>
-              <p><a href="assets/pdf/GCAmmoGrades.pdf">Ammunition Tooling Grades</a></p>
-              <p><a href="assets/pdf/GCConcreteWear.pdf">Concrete Industry Wear Parts</a></p>
-              <p><a href="assets/pdf/GCFlowControl.pdf">Erosion/Corrosion Resistance in Flow Control</a></p>
-              <p><a href="assets/pdf/GCGenFixx.pdf">GenFixx:&#8482; A Better Way to Join Tungsten Carbide & Steel</a></p>
-              <p><a href="assets/pdf/GCMetalProcessing.pdf">Metal Processing Slitters</a></p>
-              <p><a href="assets/pdf/GCPCDSubstrates.pdf">PCD Substrates and Disks</a></p>
-              <p><a href="assets/pdf/GCPowderMetalTooling.pdf">Powder Metal Tooling (wire EDM)0</a></p>
-              <p><a href="assets/pdf/GCStamping.pdf">Stamping - 4x Tool Life w/ Superior Tooling</a></p>
-              <p><a href="assets/pdf/GCRooftile.pdf">Rooftile Tooling</a></p>
-              <p><a href="assets/pdf/GCSinterHIP.pdf">Sinter-HIP Advantages</a></p>
-              <p><a href="assets/pdf/GCSteelStamping.pdf">Stamping 1006 Cold Rolled Steel</a></p>
+              <p><a href="assets/pdf/GCAmmoGrades.pdf" target="_blank">Ammunition Tooling Grades</a></p>
+              <p><a href="assets/pdf/GCConcreteWear.pdf" target="_blank">Concrete Industry Wear Parts</a></p>
+              <p><a href="assets/pdf/GCFlowControl.pdf" target="_blank">Erosion/Corrosion Resistance in Flow Control</a></p>
+              <p><a href="assets/pdf/GCGenFixx.pdf" target="_blank">GenFixx:&#8482; A Better Way to Join Tungsten Carbide & Steel</a></p>
+              <p><a href="assets/pdf/GCMetalProcessing.pdf" target="_blank">Metal Processing Slitters</a></p>
+              <p><a href="assets/pdf/GCPCDSubstrates.pdf" target="_blank">PCD Substrates and Disks</a></p>
+              <p><a href="assets/pdf/GCPowderMetalTooling.pdf" target="_blank">Powder Metal Tooling (wire EDM)</a></p>
+              <p><a href="assets/pdf/GCStamping.pdf" target="_blank">Stamping - 4x Tool Life w/ Superior Tooling</a></p>
+              <p><a href="assets/pdf/GCRooftile.pdf" target="_blank">Rooftile Tooling</a></p>
+              <p><a href="assets/pdf/GCSinterHIP.pdf" target="_blank">Sinter-HIP Advantages</a></p>
+              <p><a href="assets/pdf/GCSteelStamping.pdf" target="_blank">Stamping 1006 Cold Rolled Steel</a></p>
             </div>
             <div class="col-xs-12 col-sm-3">
               <!-- Section side content goes here -->
@@ -74,8 +74,29 @@
             </div>
           </div>
           <div class="row">
-            <div class="col-xs-12 col-sm-9">
+            <div class="technical-links col-xs-12 col-sm-9">
               <h3>Technical Articles</h3>
+              <a href="assets/pdf/GCGenFixxWhitePaper.pdf" target="_blank">An analysis of the GenFixx™ mechanical assembly technique compared to a brazed assembly technique used in severe service valve component applications</a>
+              <p class="subtext">by Thomas R. Shearer, Managing Director General Carbide|UK</p>
+              <a href="assets/pdf/GCFabTech.pdf" target="_blank">Cemented Carbides with Enhanced Functional Properties for Tooling used in Stamping Applications</a>
+              <p class="subtext">by Denis Pasay - delivered at FabTech 2010, Atlanta, GA, Nov '10</p>
+              <a href="assets/pdf/GCCementedCarbides.pdf" target="_blank">Advanced Cemented Carbides with Enhanced Functional Properties for PM Tooling Applications</a>
+              <p class="subtext">by Dr. Leonid Frayman, M. Klingensmith, V. Kruzhanov, R. Lindenau - delivered at Powder Metallurgy World Congress PM2010, Florence, Italy, Oct '10</p>
+              <a href="assets/pdf/GCPowderedMetal.pdf" target="_blank">Carbide Grades for PM Tooling</a>
+              <p class="subtext">by Mark Klingensmith and Dr. Leonid Frayman - delivered at PowderMet 2010, Hollywood, FL, June '10</p>
+              <a href="assets/pdf/GCHotRollingApplications.pdf" target="_blank">Cemented Carbide for Hot Rolling Applications</a>
+              <p class="subtext">by Dr. Leonid Frayman - delivered at PowderMet 2010, Hollywood, FL, June '10</p>
+              <a href="assets/pdf/GCLeonidSeminars.pdf" target="_blank">Advanced Cemented Carbides for Powder Metal Tooling Applications</a>
+              <p class="subtext">by Mark Klingensmith and Dr. Leonid Frayman - delivered at MPIF "PM Compacting and Tooling" seminar, Cleveland, OH, Oct '09</p>
+              <a href="assets/pdf/GCWireShow.pdf" target="_blank">Advancements in Cemented Carbide Products & Processing for the Wire Die Industry</a>
+              <p class="subtext">by Dr. Leonid Frayman, Denis Pasay - delivered at Wire Expo 2009, Cleveland, OH, Apr '09</p>
+              <a href="assets/pdf/GCAdvancementsInCemented.pdf" target="_blank">Advancements in Cemented Carbide Products & Processing</a>
+              <p class="subtext">by Dr. Leonid Frayman, Mark Klingensmith - delivered at APMI Monthly Technical Meeting, St. Marys, PA, Jan '09</p>
+              <a href="assets/pdf/GCFoundersPaper.pdf" target="_blank">The Manufacturing Process for Cemented Tungsten Carbides</a>
+              <p class="subtext">by Premo J. Pappafava, Founder of General Carbide</p>
+              <a href="assets/pdf/GCDesignerGuide.pdf" target="_blank">The Designer's Guide to Tungsten Carbide</a>
+              <p class="subtext">This publication is a reference guide for designers, engineers, fabricators and end users of tungsten carbide. It is a compilation of recommendations derived from practical experience, theoretical stress analysis, proven application engineering practices and modern manufacturing techniques. It will enable the reader to gain insight to using one of the most versatile engineering materials available today.
+              <br />© Copyright 2010 General Carbide</p>
             </div>
             <div class="col-xs-12 col-sm-3">
               <!-- Section side content goes here -->
@@ -88,8 +109,15 @@
             </div>
           </div>
           <div class="row">
-            <div class="col-xs-12 col-sm-9">
+            <div class="technical-links col-xs-12 col-sm-9">
               <h3>Ads</h3>
+              <a href="assets/pdf/GCAdPowderMetal.pdf" target="_blank">We Don't Crack Under Pressure</a>
+              <p class="subtext">As seen in International Journal of Powder Metallurgy</p>
+              <a href="assets/pdf/GCAdParts.pdf" target="_blank">All parts are not created equal. Some are made by General Carbide.</a>
+              <p class="subtext">As seen in Manufacturing Engineering magazine</p>
+              <a href="assets/pdf/GCAdPacesetters.pdf" target="_blank">Mona Pappafava-Ray</a>
+              <p class="subtext">As seen in Smart Business: Special PACESETTER edition - October 2010</p>
+              <a href="assets/pdf/GCNewsletter.pdf" target="_blank">Carbide Grade Selector E-Newsletter</a>
             </div>
             <div class="col-xs-12 col-sm-3">
               <!-- Section side content goes here -->
